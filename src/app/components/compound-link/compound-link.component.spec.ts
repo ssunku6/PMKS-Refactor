6dc1c1797c019503a406fc43496cad2f5722d288
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2023-11-14 21:48:38 -0500

    Remove boilerplate tests

--- src/app/components/compound-link/compound-link.component.spec.ts
+++ src/app/components/compound-link/compound-link.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CompoundLinkComponent } from './compound-link.component';
-
-describe('LinkComponent', () => {
-  let component: CompoundLinkComponent;
-  let fixture: ComponentFixture<CompoundLinkComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CompoundLinkComponent]
-    });
-    fixture = TestBed.createComponent(CompoundLinkComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { CompoundLinkComponent } from './compound-link.component';
+//
+// describe('LinkComponent', () => {
+//   let component: CompoundLinkComponent;
+//   let fixture: ComponentFixture<CompoundLinkComponent>;
+//
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [CompoundLinkComponent]
+//     });
+//     fixture = TestBed.createComponent(CompoundLinkComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
