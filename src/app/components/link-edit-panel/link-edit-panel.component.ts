04cb268d39ef26c342a81c35ea0e0e866bb68ec3
nicoleburgess1 <nburgess1113@gmail.com>
2023-12-13 15:30:24 -0500

    no errors :)

--- src/app/components/link-edit-panel/link-edit-panel.component.ts
+++ src/app/components/link-edit-panel/link-edit-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core'
+import { Component, OnDestroy, OnInit} from '@angular/core'
 
 
 @Component({
@@ -7,9 +7,11 @@ import { Component} from '@angular/core'
     styleUrls: ['./link-edit-panel.component.css'],
 
 })
-export class LinkEditPanelComponent {
+export class LinkEditPanelComponent{
 
     constructor(){
     }
-
+    
+    
+    
 }
