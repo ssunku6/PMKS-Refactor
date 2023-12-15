5bf7bd2a063abf544132fba148a8d8ae3ff27e19
nicoleburgess1 <nburgess1113@gmail.com>
2023-12-15 09:45:39 -0500

    visual settigns buttons

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -16,9 +16,9 @@ interface Tab {
 export class SidenavComponent {
 
     tabs: Tab[] = [
+        {selected: false, label: 'Synthesize',icon: 'assets/sidenav/synthesize.svg'},
         {selected: true, label: 'Edit',icon: 'assets/sidenav/edit.svg'},
         {selected: false, label: 'Analyze', icon:'assets/sidenav/analyze.svg'},
-        {selected: false, label: 'Synthesize',icon: 'assets/sidenav/synthesize.svg'},
     ];
     constructor(){
     }
