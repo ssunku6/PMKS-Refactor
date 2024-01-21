db0e605060cc17d85ed0e76721723602b5ca658a
RobbieEskridge <robceskridge@gmail.com>
2024-01-21 17:52:28 -0500

    we have successfully added the ability for joints to display their name dynamically in the edit panel. this is huge. we are so back.

--- src/app/components/joint-edit-panel/joint-edit-panel.component.ts
+++ src/app/components/joint-edit-panel/joint-edit-panel.component.ts
@@ -1,12 +1,14 @@
 import { Component} from '@angular/core'
+import {StateService} from "../../services/state.service";
+import {InteractionService} from "../../services/interaction.service";
+import {JointInteractor} from "../../interactions/joint-interactor"
+import {Joint} from "../../model/joint";
 
 interface Tab {
     selected: boolean,
     label: string,
     icon: string
 }
-
-
 @Component({
     selector: 'app-joint-edit-panel',
     templateUrl: './joint-edit-panel.component.html',
@@ -15,13 +17,35 @@ interface Tab {
 })
 
 export class jointEditPanelComponent {
-    constructor(){
-    }
+
   graphExpanded: { [key: string]: boolean } = {
     basicBasic: false,
     basicVisual: false,
     advancedSettingsBasic: false,
     advancedSettingsVisual: false
   };
+
+  constructor(private stateService: StateService, private interactorService: InteractionService,){
+    console.log("GraphComponent.constructor");
+  }
+
+  getCurrentJoint(){
+    let currentJointInteractor = this.interactorService.getSelectedObject();
+    return (currentJointInteractor as JointInteractor).getJoint();
+  }
+
+  getJointName(): string {
+    return this.getCurrentJoint().name + "<- here is the name of the joint";
+  }
+  setJointName(newName: string){
+    this.getCurrentJoint().name = newName;
+  }
+  getJointColor(){
+
+  }
+  setJointColor(){
+
+  }
+
 
 }
