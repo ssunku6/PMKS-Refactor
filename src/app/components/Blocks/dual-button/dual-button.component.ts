8100e89be322281c972658409d73f9f46f61314d
nicoleburgess1 <nburgess1113@gmail.com>
2024-01-29 17:23:21 -0500

    components dont work anymore, dual input and dual button added

---
+++ src/app/components/Blocks/dual-button/dual-button.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import {Link} from "../../../model/link";
+
+
+@Component({
+  selector: 'dual-button',
+  templateUrl: './dual-button.component.html',
+  styleUrls: ['./dual-button.component.scss'],
+})
+export class DualButtonComponent {
+  @Input() Link!: Link;
+  @Input() btn1Disabled: boolean = false;
+  @Input() btn2Disabled: boolean = false;
+  @Input() btn1Text: string = '';
+  @Input() btn2Text: string = '';
+  @Input() btn1Icon: string = '';
+  @Input() btn2Icon: string = '';
+  @Input() btn1Action!: () => void;
+  @Input() btn2Action!: () => void;
+
+/*
+  <mat-icon svgIcon='unweld_joint'></mat-icon>
+  <mat-icon [svgIcon]=btn3Icon></mat-icon>
+    <mat-icon svgIcon='weld_joint'></mat-icon>
+*/
+  
+  constructor() {}
+}
