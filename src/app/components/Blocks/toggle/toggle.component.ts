7fa7ee9c4f7ac9baadc4c627db114dce47e054ee
RobbieEskridge <robceskridge@gmail.com>
2024-01-29 13:51:37 -0500

    tri-button-component has been created and works for a joint. we pass it the joint and the functions that it needs to do to work, and then it does them and works. yay.

--- src/app/components/Blocks/toggle/toggle.component.ts
+++ src/app/components/Blocks/toggle/toggle.component.ts
@@ -1,21 +1,44 @@
-import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'toggle-block',
   templateUrl: './toggle.component.html',
   styleUrls: ['./toggle.component.scss'],
 })
-export class ToggleComponent {
+export class ToggleComponent implements OnInit {
   @Input() tooltip: string | undefined;
   @Input() formGroup!: FormGroup;
+  @Input() formControlGround!: FormControl;
   @Input() _formControl!: string;
 
   @Input() addInput: boolean = false;
   @Input() _formControlForInput!: string;
   @Input() disableInput: boolean = false;
 
+  @Output() toggleStateChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+
   @ViewChild('field', { static: false }) field!: ElementRef;
+
+  ngOnInit() {
+    console.log("We ran on init")
+    this.formControlGround.valueChanges.subscribe((newValue: boolean) => {
+      console.log("Trasmitting event now.");
+      this.toggleStateChanged.emit(newValue);
+      console.log("Event transmitted and state of toggle changed.");
+    });
+  }
 
   // ngOnChanges() {
   //   //Get the #field input element
--- src/app/components/joint-edit-panel/joint-edit-panel.component.ts
+++ src/app/components/joint-edit-panel/joint-edit-panel.component.ts
@@ -1,9 +1,9 @@
-import { Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {StateService} from "../../services/state.service";
 import {InteractionService} from "../../services/interaction.service";
 import {JointInteractor} from "../../interactions/joint-interactor"
 import {Joint} from "../../model/joint";
-import {FormControl, FormGroup} from "@angular/forms";
+import {Form, FormControl, FormGroup} from "@angular/forms";
 
 interface Tab {
     selected: boolean,
@@ -30,8 +30,10 @@ export class jointEditPanelComponent {
     console.log("GraphComponent.constructor");
 
   }
+  // These form control objects exist basically only to be passed in to the Dual Input Block component
   jointXFormControl: FormControl = new FormControl();
   jointYFormControl: FormControl = new FormControl();
+  jointGroundedFormControl: FormControl = new FormControl()
 
   getCurrentJoint(){
     let currentJointInteractor = this.interactorService.getSelectedObject();
@@ -44,15 +46,49 @@ export class jointEditPanelComponent {
   setJointName(newName: string){
     this.getCurrentJoint().name = newName;
   }
+
+  groundJoint(){
+    this.getCurrentJoint().addGround();
+    console.log("Ground added");
+  }
+  ungroundJoint(){
+    this.getCurrentJoint().removeGround();
+    console.log("Ground removed");
+
+  }
+
+  // Get X Coord and Y Coord return a FormControl Object. This is the kind of
+  // object required to pass into the Dual Input Block. Don't be fooled when you
+  // look into dual input block and it says "String"- it's lying to you, and if
+  // you don't give it a FormControl the values will not display.
   getJointXCoord(): FormControl {
     this.jointXFormControl.setValue(this.getCurrentJoint().coords.x)
     return this.jointXFormControl;
   }
-  // [formControl1]=getJointXCoord() [formControl2]="getJointYCoord()"
   getJointYCoord(): FormControl {
     this.jointYFormControl.setValue(this.getCurrentJoint().coords.y)
     return this.jointYFormControl;
   }
+  setJointXCoord(xCoordInput: FormControl): void {
+
+  }
+  setJointYCoord(yCoordInput: FormControl): void {
+
+  }
+
+  handleToggleStateChange(stateChange: boolean) {
+    console.log("Toggle State Changed: ", stateChange);
+    const currentJoint = this.getCurrentJoint();
+    if (stateChange) {
+      currentJoint.addGround();
+      this.jointGroundedFormControl.setValue(true);
+    }
+    else {currentJoint.removeGround();
+    this.jointGroundedFormControl.setValue(false);
+    }
+    console.log(this.getCurrentJoint().isGrounded);
+  }
+
   getJointColor(){
 
   }
