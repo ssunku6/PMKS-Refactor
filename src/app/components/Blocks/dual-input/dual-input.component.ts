4fd70c9095f538a0a131e9181ec9f34d15d1288a
RobbieEskridge <robceskridge@gmail.com>
2024-01-30 12:02:47 -0500

    got dual input blocks to work woot woot

--- src/app/components/Blocks/dual-input/dual-input.component.ts
+++ src/app/components/Blocks/dual-input/dual-input.component.ts
@@ -8,6 +8,21 @@ import {Joint} from "../../../model/joint";
   styleUrls: ['./dual-input.component.scss'],
 })
 export class DualInputComponent {
+  @Input() disabled: boolean=false;
+  @Input() tooltip: string = '';
+  @Input() input1Value: number=0;
+  @Input() input2Value: number = 0;
+  @Input() label1: string ="X";
+  @Input() label2: string ="Y";
+
+  @Output() input1Change: EventEmitter<number> = new EventEmitter<number>();
+  @Output() input2Change: EventEmitter<number> = new EventEmitter<number>();
+
+  // handle the enter key being pressed and updating the values of the input blocks
+  onEnterKeyInput1() {this.input1Change.emit(this.input1Value);}
+  onEnterKeyInput2() {this.input2Change.emit(this.input2Value);}
+
+  /*
   @Input() currentJoint!: Joint;
   @Input() tooltip!: string;
   @Input() unit!: string;
@@ -31,6 +46,7 @@ export class DualInputComponent {
   isField2Focused: boolean = false;
   showField2Overlay: boolean = false;
   lastShowField2Overlay: boolean = false;
+
 
 
   form: FormGroup = new FormGroup({
@@ -81,4 +97,121 @@ export class DualInputComponent {
   }
 
   constructor() {}
+
+
+  <div id='dual-input-block' style='{{disabled ? "cursor: default" : ""}}'>
+
+  <div class='row'>
+    <span class='label {{disabled ? "disabled" : ""}}'><ng-content></ng-content></span>
+    <mat-icon matTooltip='{{ this.tooltip }}' [matTooltipShowDelay]='1000' class='label-help'
+              [matTooltipDisabled]='disabled'
+    >help_outline
+    </mat-icon>
+    <div class='spacer'></div>
+  </div>
+
+  <div class='row' [formGroup]='formGroup' style='gap: 5px'>
+    <!--
+    <ng-container *ngIf='formSubGroup'>
+      <span class='label {{disabled ? "disabled" : ""}}'>{{label1}}</span>
+      <mat-form-field class='customInputForm'
+                      formGroupName='{{formSubGroup}}'
+                      (click)='field1.select()'
+                      (mouseenter)='isField1MouseOver = true; updateOverlay()'
+                      (mouseleave)='isField1MouseOver = false; updateOverlay()'
+                      (focusin)='isField1Focused = true; updateOverlay()'
+                      (focusout)='isField1Focused = false; updateOverlay()'>
+        <input class='customInput' matInput type='text' #field1
+               (keyup.enter)='field1.blur()'
+               [formControl]=this.field1Control>
+      </mat-form-field>
+
+      <div class='spacer'></div>
+      <span class='label {{disabled ? "disabled" : ""}}'
+            style='{{label2 === "⊾" ? "font-size: 25px; padding-bottom: 5px" : ""}}'>{{label2}}</span>
+      <mat-form-field class='customInputForm'
+                      formGroupName='{{formSubGroup}}'
+                      (click)='field2.select()'
+                      (mouseenter)='isField2MouseOver = true; updateOverlay()'
+                      (mouseleave)='isField2MouseOver = false; updateOverlay()'
+                      (focusin)='isField2Focused = true; updateOverlay()'
+                      (focusout)='isField2Focused = false; updateOverlay()'>
+        <input class='customInput' matInput type='text' #field2
+               (keyup.enter)='field2.blur()'
+               [formControl]='this.field2Control'>
+      </mat-form-field>
+    </ng-container>
+  -->
+    <ng-container *ngIf='!formSubGroup'>
+      <span class='label {{disabled ? "disabled" : ""}}'>{{label1}}</span>
+      <mat-form-field class='customInputForm'
+                      (click)='field1.select()'
+                      (mouseenter)='isField1MouseOver = true; updateOverlay()'
+                      (mouseleave)='isField1MouseOver = false; updateOverlay()'
+                      (focusin)='isField1Focused = true; updateOverlay()'
+                      (focusout)='isField1Focused = false; updateOverlay()'>
+        <input class='customInput' matInput type='text' #field1
+               (keyup.enter)='field1.blur()'
+               [formControl]='this.formControl1'>
+      </mat-form-field>
+
+      <div class='spacer'></div>
+      <span class='label {{disabled ? "disabled" : ""}}'
+            style='{{label2 === "⊾" ? "font-size: 25px; padding-bottom: 5px" : ""}}'>{{label2}}</span>
+      <mat-form-field class='customInputForm' (click)='field2.select()'
+                      (mouseenter)='isField2MouseOver = true; updateOverlay()'
+                      (mouseleave)='isField2MouseOver = false; updateOverlay()'
+                      (focusin)='isField2Focused = true; updateOverlay()'
+                      (focusout)='isField2Focused = false; updateOverlay()'>
+        <input class='customInput' matInput type='text' #field2
+               (keyup.enter)='field2.blur()'
+               [formControl]='this.formControl2'>
+      </mat-form-field>
+    </ng-container>
+
+  </div>
+</div>
+
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+}
+
+#dual-input-block {
+
+  .disabled {
+    opacity: 0.5;
+  }
+
+  .customInputForm {
+    .customInput {
+      //@include mat.typography-level($typography-config, 'body-2');
+    }
+
+    .mat-mdc-form-field-subscript-wrapper {
+      display: none;
+    }
+
+    .mat-mdc-form-field-infix {
+      padding: 6px 0;
+    }
+
+    .mdc-text-field--filled:not(.mdc-text-field--disabled) {
+      //background-color: #eff1f8;
+      background-color: #f0f1f5;
+    }
+
+    .mat-mdc-text-field-wrapper {
+      height: 30px;
+      width: 80px;
+      padding: 0 6px;
+    }
+  }
+
+}
+
+   */
 }
