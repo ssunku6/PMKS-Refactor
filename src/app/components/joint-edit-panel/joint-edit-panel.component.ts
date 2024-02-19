07a4108c780c0844bde1767fcae8ca9b6d006526
RobbieEskridge <robceskridge@gmail.com>
2024-02-19 16:42:48 -0500

    added comments on dragging

--- src/app/components/joint-edit-panel/joint-edit-panel.component.ts
+++ src/app/components/joint-edit-panel/joint-edit-panel.component.ts
@@ -37,10 +37,14 @@ export class jointEditPanelComponent {
   getMechanism(): Mechanism {return this.stateService.getMechanism();}
   getCurrentJoint() {
     let currentJointInteractor = this.interactorService.getSelectedObject();
+
+    // check the lock- disable dragging if the joint is locked, enable it if not
     if (currentJointInteractor) {
       if ((currentJointInteractor as JointInteractor).getJoint().locked) {
+        console.log("Cannot drag current selected joint!")
         currentJointInteractor.draggable = false;
       }
+      else{ currentJointInteractor.draggable = true; }
     }
     return (currentJointInteractor as JointInteractor).getJoint();
   }
